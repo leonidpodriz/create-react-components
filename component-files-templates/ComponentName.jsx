f88c3cee68ba235ac8cb6576f342e4051164a445
Leonid Podriz <leonidpodriz@gmail.com>
2020-07-26 18:52:19 +0300

    Added components files templates

---
+++ component-files-templates/ComponentName.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ComponentName.css";
+
+const ComponentName = (props) => {
+    return (
+        <div>
+            ComponentName
+        </div>
+    )
+}
+
+export default ComponentName;
